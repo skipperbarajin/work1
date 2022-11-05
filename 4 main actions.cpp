fe9c57743c9bb62839559f796920282f3fdccde3
amirreza javadi <116503361+skipperbarajin@users.noreply.github.com>
2022-11-05 10:47:29 +0330

    Add files via upload

---
+++ 4 main actions.cpp
@@ -0,0 +1,15 @@
+//crated by admin in  (1401/08/06)_(28/10/2022)
+#include<iostream>
+using namespace std;
+//this program performs 4main actions
+int main()
+{
+int a,b;
+cout<<"/nenter your number:"<<'a= ,b=' ;
+cin>>a>>b;//in put
+cout<<"/n sum:"<<(a+b);//output
+cout<<"/n difference:"<<(a-b);//output
+cout<<"/n product:"<<(a*b);//output
+cout<<"/n quotient:"<<(a/b);//output
+return 0;	
+}
---
+++ F_TO_C 1.cpp
@@ -0,0 +1,12 @@
+//this program crated by admin in 1401/08/04
+#include <iostream>
+using namespace std;
+/*this program converts F_TO_C*/
+int main()
+{
+float f;
+cout<<"/nEnter the temperature in farenheit:";
+cin>>f;//input
+cout<<"/nthe result in celsius is "<<(f-32)/1.8;//out put 
+return 0;
+}
